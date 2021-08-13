f31570e69277d6c2adbc31267f3594472c02af37
MJBOT-Discord <88763643+MJBOT-Discord@users.noreply.github.com>
2021-08-13 07:16:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const Discord = require('discord.js');
+const {Client, Intents} = require('discord.js')
  
-const client = new Discord.Client();
- 
+const client = new Client({ intents: [
+    Intents.FLAGS.GUILDS, 
+    Intents.FLAGS.GUILD_MESSAGES,
+    ] });
+
 client.once('ready', () => {
-   console.log('Ready!');
-   client.user.setActivity('?help').catch(console.error)
-});
+        console.log('Ready!');
+        client.user.setActivity('?help');
+     });
  
  
  
